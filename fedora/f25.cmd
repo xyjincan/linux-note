fda16f48e190f5ab598494293459f69cb23f090b
xyjincan <xyjincan@163.com>
2017-08-12 11:41:20 +0800

    使用历史

---
+++ fedora/f25.cmd
@@ -0,0 +1,857 @@
+    1  ls
+    2  pwd
+    3  source /etc/profile
+    4  java
+    5  ls
+    6  cd ..
+    7  sudo mkdir com
+    8  chmod a+wr com
+    9  sudo chmod a+wr com
+   10  ls
+   11  cd com/
+   12  ls
+   13  pwd
+   14  ls
+   15  sudo kwrite /etc/profile
+   16  ls
+   17  ls
+   18  tar -xzf de.tar.gz -C /home/com
+   19  ls
+   20  sudo dnf update -y
+   21  ls
+   22  cd /home/
+   23  ls
+   24  cd com/
+   25  lls
+   26  ls
+   27  ls -al
+   28  cp * /home/f1/
+   29  ls
+   30  cd ~
+   31  ls -al
+   32  tar -xzf AndroidSdk.tar.gz -C /home/com
+   33  cd /home/com
+   34  ls
+   35  cd AndroidSdk/
+   36  ls
+   37  sudo dnf install gcc
+   38  sudo kill 2737
+   39  sudo dnf update -y
+   40  sudo dnf upgrade -y
+   41  cd /etc/
+   42  ls
+   43  cd yum.repos.d/
+   44  ls
+   45  pwd
+   46  cd ~
+   47  ls
+   48  mv repo.txt repo.sh
+   49  mv repo.sh /etc/yum.repos.d/
+   50  sudo mv repo.sh /etc/yum.repos.d/
+   51  cd  /etc/yum.repos.d/
+   52  ls
+   53  cat repo.sh 
+   54  su
+   55  sudo dnf update -y
+   56  pwd
+   57  sudo dnf update -y
+   58  cd ~
+   59  ls
+   60  cd dd/
+   61  ls
+   62  cd ..
+   63  rm -rf dd
+   64  ls
+   65  ls -al
+   66  ls
+   67  ls -al
+   68  ls
+   69  ./dk 
+   70  git status
+   71  mvn
+   72  vim /etc/profile
+   73  vi /etc/profile
+   74  sudo vi /etc/profile
+   75  source /etc/profile
+   76  ls -al
+   77  ls -a
+   78  cd de
+   79  ls
+   80  cd ..
+   81  rm -rf de
+   82  ls
+   83  sl
+   84  ls
+   85  cd fedora/
+   86  ls -al
+   87  ls
+   88  cp * ~
+   89  cp -R * ~
+   90  cp -r * ~
+   91  ls
+   92  ls -a dd
+   93  ls
+   94  cd dd/
+   95  ls
+   96  ls -al
+   97  cp * ~/
+   98  cp -r * ~/
+   99  ls
+  100  cd ..
+  101  ls
+  102  ls
+  103  pwd
+  104  rpm -qpl mysql-community-server-version-distribution-arch.rpm
+  105  rpm -qpl mysql-community-{server,client,common,libs}-* mysql-5.* 
+  106  rpm -ivq mysql-community-{server,client,common,libs}-* mysql-5.* 
+  107  rpm -ivq mysql-community-{server,client,common,libs}-*
+  108  rpm -ivq mysql-community-common-5.7.17-1.fc25.x86_64.rpm 
+  109  cd /etc/
+  110  ls
+  111  cd yum.repos.d/
+  112  ls
+  113  mkdir bk
+  114  sudo mkdir bk
+  115  cp * bk
+  116  sudo cp * bk
+  117  ls
+  118  rm *po
+  119  sudo rm *po
+  120  ls
+  121  pwd
+  122  mysql
+  123  mysql -uroot -p
+  124  rpm -qa | grep mariadb
+  125  dnf -erase
+  126  dnf -erase mariadb-server-10.1.18-1.fc25.x86_64
+  127  mariadb-common-10.1.18-1.fc25.x86_64
+  128  mariadb-config-10.1.18-1.fc25.x86_64
+  129  mariadb-libs-10.1.18-1.fc25.x86_64
+  130  mariadb-embedded-10.1.18-1.fc25.x86_64
+  131  mariadb-10.1.18-1.fc25.x86_64
+  132  mariadb-errmsg-10.1.18-1.fc25.x86_64
+  133  dnf -erase mariadb-server-10.1.18-1.fc25.x86_64
+  134  rpm -qa | grep mariadb
+  135  dnf --help
+  136  dnf --erase
+  137  rpm -qa | grep mariadb
+  138  dnf -e
+  139  dnf -erase
+  140  dnf --erase
+  141  dnf -erase
+  142  rpm -qa | grep mariadb
+  143  yum -remove
+  144  dnf -rmm
+  145  dnf -rm
+  146  rpm -qa | grep mariadb
+  147  sudo dnf remove mariadb-common-10.1.18-1.fc25.x86_64
+  148  rpm -qa | grep mariadb
+  149  ls
+  150  sudo cp *.repo /etc/yum.repos.d
+  151  cd  /etc/yum.repos.d
+  152  ls
+  153  rm *163*
+  154  sudo rm *163*
+  155  ls
+  156  sudo rm *sohu*
+  157  ls
+  158  sudo rm *cisco*
+  159  ls
+  160  rm repo.sh 
+  161  sudo rm repo.sh 
+  162  ls
+  163  cd ~
+  164  sudo dnf update -y
+  165  ping mirrors.aliyun.com
+  166  ping mirrors.163.com
+  167  ls
+  168  cd mysql-5.7.17-1.fc25.x86_64.rpm-bundle/
+  169  ls
+  170  dnf install mysql-community-common-5.7.17-1.fc25.x86_64.rpm 
+  171  sudo dnf install mysql-community-common-5.7.17-1.fc25.x86_64.rpm 
+  172  sudo rpm install mysql-community-common-5.7.17-1.fc25.x86_64.rpm 
+  173  sudo dnf install mysql-community-common-5.7.17-1.fc25.x86_64.rpm 
+  174  ls
+  175  cp *.repo /etc/yum.repos.d/
+  176  sudo cp *.repo /etc/yum.repos.d/
+  177  sudo dnf update -y
+  178  ls
+  179  cd ~/note/
+  180  vi repo 
+  181  ls
+  182  cd ~
+  183  cd note/
+  184  vim repo
+  185  vi repo
+  186  sudo dnf install gcc g++ git clang wget tree make vim
+  187  sudo dnf install gcc-c++ git clang wget tree make vim
+  188  vim
+  189  ls
+  190  cd al/
+  191  ls
+  192  cat fedora-cisco-openh264.repo 
+  193  ls
+  194  cd ..
+  195  ls
+  196  sdk
+  197  cd /etc/yum.repos.d/
+  198  ls
+  199  sudo mkdir al
+  200  ls
+  201  mv *repo* al
+  202  sudo mv *repo* al
+  203  ls
+  204  cp bk/* .
+  205  sudo cp bk/* .
+  206  ls
+  207  sudo dnf update -y
+  208  ls
+  209  reboot
+  210  su
+  211  dnf list 
+  212  ls
+  213  cd ~/note/
+  214  ls
+  215  cat repo 
+  216  cd  < cat repo
+  217  cd  << cat repo
+  218  ls
+  219  cd /etc/yum.repos.d/
+  220  ls
+  221  sudo rpm -Uhv http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-25.noarch.rpm
+  222  sudo rpm -Uhv http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-25.noarch.rpm
+  223  ls  netease-cloud-music-1.0.0-2.x86_64.rpm
+  224  sudo rpm -i netease-cloud-music-1.0.0-2.x86_64.rpm --replacefiles
+  225  ls
+  226  ls
+  227  cd /
+  228  l
+  229  ll
+  230  cd ~
+  231  ls
+  232  dnf config-manager --add-repo=http://repo.fdzh.org/FZUG/FZUG.repo
+  233  sudo dnf config-manager --add-repo=http://repo.fdzh.org/FZUG/FZUG.repo
+  234  sudo dnf update
+  235  sudo dnf update -y
+  236  ls
+  237  cd 下载
+  238  ls
+  239  rm fedora.repo 
+  240  rm fedora-updates.repo 
+  241  ls
+  242  ls * rpm
+  243  ls
+  244  ls rpmfusion-free-release-25.noarch.rpm 
+  245  sudo rpm -i rpmfusion-free-release-25.noarch.rpm 
+  246  sudo dnf update -y
+  247  sudo dnf install gstreamer vlc ffmpeg ffmpeg-libs gstreamer-ffmpeg xvidcore libdvdread libdvdnav lsdvd gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-bad gstreamer1-plugins-ugly libmpg123
+  248  sudo dnf update -y
+  249  ls
+  250  sudo dnf update -y
+  251  ls
+  252  cd ~/note/
+  253  ls
+  254  git status
+  255  git add
+  256  git add .
+  257  ls
+  258  git status
+  259  git commit -m "I am back"
+  260  git config --global user.email "xyjincan@163.com"
+  261  git config --global user.name "xyjincan"
+  262  git commit -m "I am back"
+  263  git push origin master 
+  264  reboot
+  265  mvn
+  266  ls
+  267  ./dk 
+  268  ls
+  269  ./dk 
+  270  dnf install google-chrome-stable 
+  271  sudo dnf install google-chrome-stable 
+  272  wget https://repo.fdzh.org/chrome/google-chrome-mirrors.repo -P /etc/yum.repos.d/  # Fedora/RHEL  
+  273  sudo wget https://repo.fdzh.org/chrome/google-chrome-mirrors.repo -P /etc/yum.repos.d/  # Fedora/RHEL  
+  274  sudo dnf install xx-net
+  275  ./xx-net 
+  276  xx-net 
+  277  sudo dnf erase firefox
+  278  sudo dnf erase konqueror qupzilla
+  279  sudo dnf install kate
+  280  kate
+  281  ls
+  282  ls -a
+  283  reboot
+  284  ls
+  285  cp 吴金灿-java-2017.pdf ~
+  286  cd ~
+  287  ls
+  288  chmod a-x 吴金灿-java-2017.pdf
+  289  ls
+  290  scp 吴金灿-java-2017.pdf root@watchers.cc
+  291  scp 吴金灿-java-2017.pdf root@watchers.cc:~
+  292  ssh watchers.cc
+  293  ssh root@watchers.cc
+  294  ls
+  295  reboot
+  296  sudo dnf update -y
+  297  ls
+  298  ./server 
+  299  ls
+  300  gcc server.c 
+  301  rm server
+  302  ls
+  303  mv a.out server
+  304  ./server 
+  305  ls
+  306  ./client 
+  307  ./server 
+  308  LS
+  309  gcc client.c 
+  310  ./a.out 
+  311  ls
+  312  ./a.out 
+  313  ls
+  314  touch server.c
+  315  kate server.c 
+  316  mv a.out client
+  317  gcc server.c 
+  318  ls
+  319  mv a.out server
+  320  ls
+  321  ./client 
+  322  ls
+  323  ./client 
+  324  ls
+  325  history >> his.txt
+  326  ls
+  327  ./client 
+  328  ls
+  329  ./client 
+  330  ls
+  331  gcc Nclient.c 
+  332  ./a.out 
+  333  mv NotoSansCJK.ttc.zip ~
+  334  cd ~
+  335  ls
+  336  rm *.dpf
+  337  rm *.pdf
+  338  ls
+  339  unzip NotoSansCJK.ttc.zip 
+  340  cd N
+  341  ls
+  342  dnf erase filefox
+  343  sudo dnf erase filefox
+  344  sudo dnf erase firefox
+  345  sudo dnf erase apper
+  346  kcmshell4 fonts
+  347  linux -a
+  348  ulinux
+  349  screenfetch
+  350  linux 
+  351  uname -a
+  352  uname -l
+  353  uname --help
+  354  uname -s
+  355  uname -n
+  356  uname -a
+  357  rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  358  sudo rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  359  ./server 
+  360  ./a.out 
+  361  a
+  362  ./a.out 
+  363  sudo rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpml
+  364  ls
+  365  gcc Nclient.c 
+  366  ./a.out 
+  367  gcc Nclient.c 
+  368  ./a.out 
+  369  gcc Nclient.c 
+  370  ./a.out 
+  371  gcc Nclient.c 
+  372  ./a.out 
+  373  gcc Nclient.c 
+  374  ./a.out 
+  375  ls
+  376  ./a.out 
+  377  sss
+  378  ./a.out 
+  379  bash: sss: 未找到命ssssssssss令
+  380  ./a.out 
+  381  ls
+  382  rm client.c 
+  383  rm client 
+  384  ls
+  385  mv Nclient.c client
+  386  mv  client client.c
+  387  ls
+  388  rm his.txt 
+  389  ls
+  390  rm a.out 
+  391  ls
+  392  rm server
+  393  ls
+  394  konsole
+  395  ls
+  396  reboot
+  397  ps -aux
+  398  ps -aux | grep python
+  399  sudo kill 72288
+  400  sudo kill 2461116
+  401  ps -aux | grep python
+  402  ps -aux | grep xx-net
+  403  sudo dnf update -y
+  404  sudo dnf update 
+  405  sudo dnf update -y
+  406  sudo dnf erase wps-office
+  407  sudo dnf insatll wps-office
+  408  sudo dnf insatl wps-office
+  409  sudo dnf instal wps-office
+  410  sudo dnf -instal wps-office
+  411  sudo dnf install wps-office
+  412  sudo dnf instal wps-office
+  413  ipconfig
+  414  ifconfig
+  415  cd http/
+  416  ls
+  417  cd socket/
+  418  ls
+  419  cd tcp/
+  420  ls
+  421  touch DieWithMessage.h
+  422  touch TCPClientUtility.c
+  423  touch client.c
+  424  kate client.c 
+  425  kate DieWithMessage.h
+  426  kate TCPClientUtility.c
+  427  ls
+  428  gcc client.c 
+  429  ./a.out 
+  430  ./a.out 127.0.0.1 mmd
+  431  ./a.out 127.0.0.1 11101 mmd
+  432  ./a.out 127.0.0.1:8080 11101 mmd
+  433  nlimit -n
+  434  gcc client.c 
+  435  ls
+  436  ./a.out 
+  437  ./a.out 127.0.0.1
+  438  gcc client.c 
+  439  ./a.out 127.0.0.1
+  440  gcc client.c 
+  441  ls
+  442  rm a.out 
+  443  ls
+  444  rm DieWithMessage.h TCPClientUtility.c 
+  445  ls
+  446  rm shshhs 
+  447  ls
+  448  cd ..
+  449  tree
+  450  ls
+  451  cd ..
+  452  ls
+  453  cd com/
+  454  ls
+  455  cd eclipse/
+  456  ls
+  457  pwd
+  458  dolphin
+  459  ls
+  460  ls
+  461  cd 桌面
+  462  ls
+  463  cp Intellij-idea.desktop eclipse
+  464  cp eclipse eclipse.desktop
+  465  ls
+  466  kate eclipse.desktop 
+  467  ls
+  468  vim eclipse.desktop 
+  469  kate eclipse.desktop 
+  470  cd .m2
+  471  ls
+  472  cp -r * /home/f1/.m2/
+  473  mvn
+  474  ls
+  475  cd .m2
+  476  ls
+  477  cd repository/
+  478  ls
+  479  cd ..
+  480  ls
+  481  rm -rf *
+  482  ls
+  483  pwd
+  484  ls
+  485  mysql
+  486  cd note/
+  487  ls
+  488  cat mysql.txt 
+  489  mysql -uroot -p
+  490  ./dk
+  491  dnf erase mysql
+  492  sudo dnf erase mysql
+  493  systemctl mysql start
+  494  systemctl server mysql start
+  495  systemctl start mysql start
+  496  ls
+  497  systemctl start mysql enable
+  498  cd /root/
+  499  su
+  500  mysql_secure_installation
+  501  ls
+  502  mysql_secure_installation
+  503  su
+  504  systemctl start mysql enable
+  505  systemctl start mysql 
+  506  ls
+  507  sudo dnf erase Maradb
+  508  sudo dnf erase mysql
+  509  sudo dnf erase mariadb
+  510  mysql
+  511  ls *.zip
+  512  cp apache-tomcat*.zip /home/com/tomcat/
+  513  cd /home/com/tomcat/
+  514  ls
+  515  unzip apache-tomcat-8*.zip
+  516  unzip apache-tomcat-8.5.13.zip 
+  517  ls
+  518  unzip apache-tomcat-8.0.42.zip 
+  519  ls
+  520  sudo dnf install rabbitmq
+  521  sudo dnf install rabbit
+  522  cd /home/com/
+  523  ls
+  524  cd tomcat/
+  525  ls
+  526  pwd
+  527  ls
+  528  unzip apache-tomcat-9.0.0.M19.zip 
+  529  ls
+  530  ls
+  531  rpm -r *.rpm
+  532  rpm -i *.rpm
+  533  rpm -i mysql-community-common-5.7.17-1.fc25.x86_64.rpm
+  534  sudo rpm -i mysql-community-common-5.7.17-1.fc25.x86_64.rpm
+  535  ls
+  536  sudo yum install mysql-community-{server,client,common,libs}-* 
+  537  ls
+  538  cd ~
+  539  ls
+  540  sudo grep 'temporary password' /var/log/mysqld.log
+  541  sudo grep 'temporary password' /var/log/mysql/mysqld.log
+  542  sudo service mysql enable
+  543  sudo systemctl enable mysqld
+  544  sudo systemctl start mysqld
+  545  sudo grep 'temporary password' /var/log/mysql/mysqld.log
+  546  sudo grep 'temporary password' /var/log/mysqld.log
+  547  mysql -uroot -p
+  548  mysql -uroot -pFedora25!
+  549  ulimit
+  550  ulimit -S -n 
+  551  ulimit -S -n 4096
+  552  ulimit -S -n 
+  553  sudo vim /etc/security/limits.conf 
+  554  ls
+  555  ls
+  556  mv *.rpm data/
+  557  cd data/
+  558  ls
+  559  cd ..
+  560  ls
+  561  mv *.zip data
+  562  ls
+  563  cd data/
+  564  ls
+  565  cp apache-tomcat-9.0.0.M19.zip /home/com/tomcat
+  566  ls
+  567  ls apache-tomcat-*
+  568  rm apache-tomcat-*
+  569  ls
+  570  rpm -i rabbitmq-server-3.6.9-1.el7.noarch.rpm 
+  571  sudo dnf install erlang
+  572  rpm -i rabbitmq-server-3.6.9-1.el7.noarch.rpm 
+  573  sudo rpm -i rabbitmq-server-3.6.9-1.el7.noarch.rpm 
+  574  ls
+  575  systemctl enable rabbitmq
+  576  systemctl enable rabbitmq-server.service 
+  577  systemctl start rabbitmq-server.service 
+  578  rabbitmqctl status
+  579  sudo rabbitmqctl status
+  580  cd ~
+  581  ./dk 
+  582  ls
+  583  cd /var/lib/
+  584  ls
+  585  cd rabbitmq/
+  586  ls
+  587  cd /etc/rabbitmq/
+  588  ls
+  589  cd ~
+  590  ls
+  591  ls -a
+  592  sudo dnf update -y
+  593  dnf install rabbitmq-server
+  594  sudo dnf install rabbitmq-server
+  595  mv *.html page
+  596  ls
+  597  sudo dnf erase calligra
+  598  sudo dnf erase calligra*
+  599  sudo dnf erase calligra-*
+  600  ls
+  601  rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  602  sudo rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  603  sudo rpm -i wps-office-fonts-1.0-1.noarch.rpm 
+  604  ls
+  605  mv eclipse-jee-neon-3-linux-gtk-x86_64\ \(1\).tar.gz /home/com/
+  606  ls
+  607  cp /home/com/
+  608  cd /home/com/
+  609  ls
+  610  mv eclipse eclipseo
+  611  ls
+  612  tar -xzf eclipse-jee-neon-3-linux-gtk-x86_64\ \(1\).tar.gz 
+  613  ls
+  614  rm -rf eclipseo
+  615  ls
+  616  shutdown -h now
+  617  mv ideaIU-2017.1.tar.gz /home/com/
+  618  cd ~/../com/
+  619  ls
+  620  rm eclipse-jee-neon-3-linux-gtk-x86_64\ \(1\).tar.gz 
+  621  ls
+  622  tar -xzf ideaIU-2017.1.tar.gz 
+  623  ls
+  624  rm -rf idea-IU-163.13906.18
+  625  ls
+  626  rm -rf *.gz
+  627  ls
+  628  cd oe/
+  629  ls
+  630  cd ...
+  631  ls
+  632  cd ..
+  633  ls
+  634  rm -rf oe
+  635  ls
+  636  mvn
+  637  ls
+  638  mvn --help
+  639  mvn -V
+  640  mvn -v
+  641  mvn -V
+  642  mvn -v
+  643  sudo dnf update -y
+  644  ping qianss.xyz
+  645  mysql -uroot -p
+  646  cd /usr/lib/rabbitmq/
+  647  ls
+  648  cd bin/
+  649  ls
+  650  sudo rabbitmq-plugins enable rabbitmq_managenment
+  651  sudo rabbitmq-plugins enable rabbitmq_management
+  652  cd ~
+  653  ls
+  654  cd work/
+  655  ls
+  656  cd 资料
+  657  ls
+  658  vim rabbitmq 
+  659  mv 资料 ref
+  660  cd ..
+  661  ls
+  662  mv 资料 ref
+  663  ls
+  664  reboot
+  665  cd /usr/lib/rabbitmq/
+  666  ls
+  667  cd lib/
+  668  ls
+  669  cd rabbitmq_server-3.6.9/
+  670  ls
+  671  cd sbin/
+  672  ls
+  673  cd ..
+  674  ls
+  675  cd ebin/
+  676  ls
+  677  cd ..
+  678  ls
+  679  cd include/
+  680  ls
+  681  cd ..
+  682  ls
+  683  cd /etc/rabbitmq/
+  684  ls
+  685  cd ~
+  686  ls
+  687  ls -a
+  688  ls
+  689  java -version
+  690  uname -a
+  691  shutdown -h now
+  692  cd .m2/
+  693  cd .m2/
+  694  ls
+  695  shutdown -h now
+  696  cd note/
+  697  ls
+  698  ls nginx 
+  699  cat n
+  700  LS
+  701  ls
+  702  ls nginx 
+  703  cat nginx 
+  704  ssh watchers.cc
+  705  ssh jc@watchers.cc
+  706  ssh root@watchers.cc
+  707  ssh jc@watchers.cc
+  708  ssh root@watchers.cc
+  709  ls
+  710  cd 下载
+  711  ls
+  712  scp all.tar.gz jc@watchers.cc:~
+  713  ls
+  714  ssh jc@watchers.cc
+  715  ls
+  716  ssh root@watchers.cc
+  717  ls
+  718  ssh root@watchers.cc
+  719  ld
+  720  ls
+  721  ls
+  722  cd /home/com
+  723  ls
+  724  cd /
+  725  cat etc/profile
+  726  cat /etc/profile
+  727  ls
+  728  sudo dnf update
+  729  sudo dnf -y update
+  730  uname -a
+  731  sudo dnf update -y
+  732  sudo dnf erase wps
+  733  sudo dnf erase wps*
+  734  sudo dnf erase wps-office
+  735  sudo dnf update -y
+  736  su
+  737  ps -aux
+  738  ps -aux | grep xx-net
+  739  ps -aux | grep "xx-net"
+  740  ls
+  741  cd ..
+  742  ls
+  743  cd gae_proxy/
+  744  ls
+  745  cd /boot/
+  746  ls
+  747  cd /root/
+  748  ls
+  749  cd root
+  750  cd /root
+  751  ls
+  752  su
+  753  cd .xx-net/
+  754  ls
+  755  cd gae_proxy/
+  756  ls
+  757  rm CA.crt certs/
+  758  rm -rf CA.crt certs/
+  759  ls
+  760  ps -aux | grep xx-net
+  761  su
+  762  xx-net
+  763  sudo dnf install pyOpenSSL.noarch
+  764  sudo dnf update -y
+  765  cd /etc/yum.repos.d/
+  766  ls
+  767  cat FZUG.repo 
+  768  ls
+  769  sudo kate FZUG.repo 
+  770  sudo dnf install wps
+  771  sudo dnf install wps*
+  772  ls
+  773  cd note/
+  774  ls
+  775  cat new-fedora24 
+  776  sudo dnf insatll wps*
+  777  sudo dnf install wps*
+  778  wget http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office-10.1.0.5672-1.a21.x86_64.rpm
+  779  ls
+  780  rm wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  781  ls
+  782  cat xx-net 
+  783  ls
+  784  unzip XX-Net-master.zip 
+  785  sh xx_net.sh 
+  786  sudo  xx_net.sh 
+  787  su
+  788  rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  789  sudo rpm -i wps-office-10.1.0.5672-1.a21.x86_64.rpm 
+  790  sudo dnf update-y
+  791  sudo dnf update -y
+  792  abd
+  793  uname -a
+  794  reboot
+  795  ipconfig
+  796  ip
+  797  ipconfig
+  798  ifconfig
+  799  su
+  800  reboot
+  801  sudo dnf update -y
+  802  sudo dnf update
+  803  sudo dnf update -y
+  804  sudo dnf update 
+  805  ping www.baidu.com
+  806  ipconfig
+  807  ifconfig
+  808  ipconfig
+  809  ping www.baidu.com
+  810  ipconfig
+  811  ping www.baidu.com
+  812  sudo dnf update -y
+  813  sudo dnf update
+  814  sudo ping qq.com
+  815  ifconfig
+  816  ping 223.5.5.5
+  817  ping 119.29.29.29
+  818  ping 223.5.5.5
+  819  ping 119.29.29.29
+  820  cat /etc/resolv.conf 
+  821  sudo vim /etc/resolv.conf 
+  822  source /etc/resolv.conf 
+  823  ifconfig
+  824  sudo dnf update
+  825  sudo kill 4090
+  826  sudo dnf update
+  827  cd system32
+  828  ls
+  829  sudo vim /etc/host
+  830  sudo vim /etc/hosts
+  831  source /etc/hosts
+  832  ls
+  833  w3m
+  834  bash
+  835  sudo dnf update -y
+  836  su
+  837  history
+  838  su
+  839  sudo grub2
+  840  sudo grub2-mkconfig 
+  841  sudo grub2
+  842  sudo grub2-mkconfig 
+  843  reboot
+  844  sudo dnf update -y
+  845  su
+  846  reboot
+  847  sudo dnf update -y
+  848  su
+  849  ls
+  850  cd http/
+  851  ls
+  852  ab
+  853  sudo dnf install ab
+  854  sudo dnf install backbench 
+  855  sudo dnf install screen
+  857  sudo dnf install httpd-tools
+  858  sudo dnf clean packpages
